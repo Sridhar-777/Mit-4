ebfc363adca7b6893bfbcdf724af61ec4329fba5
Sridhar.R <164163973+Sridhar-777@users.noreply.github.com>
2024-05-18 11:28:37 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,16 +1,20 @@
 document.getElementById('add').addEventListener('click', function() {
     var name = document.getElementById('input').value;
     var email = document.getElementById('mail').value;
+
     // var pass=sridhar@gmail.com
 
     if (name && email  ) {
         // Create a new list item
         var para= document.createElement('p');
+        var btn=document.createElement('button')
         para.textContent = `NAME: ${name},  EMAIL:${email}`;
+        btn.innerHTML="delete"
         
 
         // Append the new item to the contact list
         document.getElementById('contactList').appendChild(para);
+        contactList.appendChild(btn)
 
         // Clear the input fields
         document.getElementById('input').value = '';
@@ -19,4 +23,6 @@ document.getElementById('add').addEventListener('click', function() {
         alert('Please enter both name and email address.');
     }
 });
+
+
 
